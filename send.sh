6aba905eb649f6987195fe4f6a6906b592b39291
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-03-30 16:25:00 +0200

    ✨ [SSH-key] Add ssh-key before send backup to preprod

--- send.sh
+++ send.sh
@@ -5,8 +5,14 @@ echo 'Starting database backup dump send to preprod';
 . ./.passwords.sh
 sh ./start_timestamp.sh
 
+echo 'Adding ssh-key to keychain';
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_ed25519
+ssh-add -l
+echo 'Identity added';
+
 echo 'Copying dump to preprod';
-scp $LAST_DUMP_PATH $PREPROD_USER@$PREPROD_IP:"~/$PREPROD_DUMP_NAME"
+scp $LAST_DUMP_PATH $PREPROD_USER@$PREPROD_IP:$HOME/dump.sql
 echo 'Dump copied';
 
 sh ./end_timestamp.sh
