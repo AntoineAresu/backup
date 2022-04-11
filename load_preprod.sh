3fc613f9763969cc7c33bf8d15118da83a19b8a9
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-04-11 18:15:40 +0200

    ✨ [Load preprod] Add migration

--- load_preprod.sh
+++ load_preprod.sh
@@ -7,6 +7,7 @@ sh ./start_timestamp.sh
 
 echo 'Loading dump to preprod database';
 mysql --host=$MYSQL_HOST --port=$MYSQL_PORT -u $MYSQL_PREPROD_USER $MYSQL_PREPROD_DB -p$MYSQL_PREPROD_PASSWORD < $HOME/dump.sql;
+php $HOME/www/current/bin/console app:anonymize-db doctrine:migrations:migrate -n
 echo 'Dump copied';
 
 sh ./end_timestamp.sh
