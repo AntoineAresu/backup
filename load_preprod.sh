e72b2843b83cb15cc1a37eb17658bec13fd0e46c
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-04-11 18:34:29 +0200

    ✨ [Migrations] Fix wrong folder problem

--- load_preprod.sh
+++ load_preprod.sh
@@ -7,7 +7,7 @@ sh ./start_timestamp.sh
 
 echo 'Loading dump to preprod database';
 mysql --host=$MYSQL_HOST --port=$MYSQL_PORT -u $MYSQL_PREPROD_USER $MYSQL_PREPROD_DB -p$MYSQL_PREPROD_PASSWORD < $HOME/dump.sql;
-php $HOME/www/current/bin/console doctrine:migrations:migrate -n
+cd $HOME/www/current && php ./bin/console doctrine:migrations:migrate -n && cd -
 echo 'Dump copied';
 
 sh ./end_timestamp.sh
